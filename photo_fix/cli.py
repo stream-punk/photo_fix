0757a2cb358c782c309f3aec5ebafc6961160434
Streampunk <glad.car1474@fastmail.com>
2021-07-21 00:03:13 +0200

    compare funcs

--- photo_fix/cli.py
+++ photo_fix/cli.py
@@ -12,13 +12,31 @@ from .hash import hash_dir
 
 
 def compressed_pickle(file_, data):
+    if not file_.suffix == ".pbz2":
+        raise ValueError("file must have the .pbz2 extension")
     with bz2.BZ2File(str(file_), "wb") as f:
         pickle.dump(data, f)
 
 
 def decompress_pickle(file_):
+    if not file_.suffix == ".pbz2":
+        raise ValueError("file must have the .pbz2 extension")
     with bz2.BZ2File(file_, "rb") as f:
         return pickle.load(f)
+
+
+def dump(directory, images):
+    print(json.dumps([str(Path(directory, image).resolve()) for image in images]))
+
+
+def check_hashes(reference, compare, func):
+    reference = Path(reference)
+    compare = Path(compare)
+    _, reference = decompress_pickle(reference)
+    directory, compare = decompress_pickle(compare)
+    for hash, images in compare.items():
+        if func(hash, reference):
+            dump(directory, images)
 
 
 @click.group()
@@ -49,18 +67,44 @@ def ihash(directory, output):
 
 
 @run.command()
-@click.argument("input", type=click.Path(readable=True, dir_okay=False, file_okay=True))
+@click.argument(
+    "input",
+    type=click.Path(
+        exists=True,
+        readable=True,
+        dir_okay=False,
+        file_okay=True,
+    ),
+)
 def duplicates(input):
-    directory, images = decompress_pickle(input)
-    for images in images.values():
+    input = Path(input)
+    directory, input = decompress_pickle(input)
+    for images in input.values():
         if len(images) > 1:
-            print(
-                json.dumps([str(Path(directory, image).resolve()) for image in images])
-            )
+            dump(directory, images)
 
 
 @run.command()
-@click.argument("reference")
-@click.argument("clean")
-def compare(reference, clean):
-    pass
+@click.argument(
+    "reference",
+    type=click.Path(exists=True, readable=True, dir_okay=False, file_okay=True),
+)
+@click.argument(
+    "compare",
+    type=click.Path(exists=True, readable=True, dir_okay=False, file_okay=True),
+)
+def not_in_ref(reference, compare):
+    check_hashes(reference, compare, lambda hash, reference: hash not in reference)
+
+
+@run.command()
+@click.argument(
+    "reference",
+    type=click.Path(exists=True, readable=True, dir_okay=False, file_okay=True),
+)
+@click.argument(
+    "compare",
+    type=click.Path(exists=True, readable=True, dir_okay=False, file_okay=True),
+)
+def in_ref(reference, compare):
+    check_hashes(reference, compare, lambda hash, reference: hash in reference)
