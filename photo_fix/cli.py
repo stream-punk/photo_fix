5f6a98115362beb807bc79c336228d9b9aa15790
Streampunk <glad.car1474@fastmail.com>
2021-07-20 22:34:57 +0200

    hashing works

--- photo_fix/cli.py
+++ photo_fix/cli.py
@@ -1,8 +1,59 @@
+import bz2
+import pickle
+from collections import defaultdict
+from pathlib import Path
+
 import click
 
+from .hash import hash_dir
 
-@click.command()
+
+def compressed_pickle(file_, data):
+    with bz2.BZ2File(str(file_), "wb") as f:
+        pickle.dump(data, f)
+
+
+def decompress_pickle(file_):
+    with bz2.BZ2File(file_, "rb") as f:
+        return pickle.load(f)
+
+
+@click.group()
+def run():
+    pass
+
+
+@run.command()
+@click.argument(
+    "directory",
+    type=click.Path(
+        exists=True,
+        dir_okay=True,
+        file_okay=False,
+        readable=True,
+    ),
+)
+@click.argument(
+    "output", type=click.Path(writable=True, dir_okay=False, file_okay=True)
+)
+def ihash(directory, output):
+    directory = Path(directory)
+    images = defaultdict(list)
+    hash_dir(directory, images)
+    compressed_pickle(output, images)
+
+
+@run.command()
+@click.argument("input", type=click.Path(readable=True, dir_okay=False, file_okay=True))
+def duplicates(input):
+    images = decompress_pickle(input)
+    for images in images.values():
+        if len(images) > 1:
+            print(images)
+
+
+@run.command()
 @click.argument("reference")
 @click.argument("clean")
-def run():
-    print("huh")
+def compare(reference, clean):
+    pass
--- photo_fix/hash.py
+++ photo_fix/hash.py
@@ -1,1 +1,31 @@
+from multiprocessing import Pool
+from pathlib import Path
 
+import imagehash
+from PIL import Image
+
+ignore = [".mp4", ".mov"]
+
+
+def hash_image(item):
+    img = Image.open(bytes(item))
+    hash = imagehash.average_hash(img)
+    return (hash, item)
+
+
+def find(dir: Path, images):
+    for item in dir.iterdir():
+        if item.is_dir():
+            find(item, images)
+        elif not item.is_symlink():
+            if not item.suffix.lower() in ignore:
+                images.append(item)
+
+
+def hash_dir(dir: Path, images):
+    image_list = []
+    find(dir, image_list)
+    with Pool() as pool:
+        image_list = pool.map(hash_image, image_list)
+    for hash, item in image_list:
+        images[hash].append(item)
